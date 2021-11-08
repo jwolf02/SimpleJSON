b53ef2447f6b06b92fb7e57ecb86b581c80b2daa
Jonas Wolf <jonas.wolf@ida-analytics.de>
2021-11-08 15:35:20 +0100

    refactored implementation to use streams instead of iterators

--- lib/SimpleJSON.cpp
+++ lib/SimpleJSON.cpp
@@ -1,6 +1,7 @@
 #include <SimpleJSON.hpp>
 #include <stdexcept>
 #include <FSMStates.hpp>
+#include <sstream>
 
 typedef const char*   Iterator;
 
@@ -22,24 +23,28 @@ static bool _expected(char c, State state) {
     return false;
 }
 
-static char _get_next_character(Iterator &begin, Iterator &end, int state) {
-    while (begin != end && (*begin == ' ' || *begin == '\t' || *begin == '\n')) {
-        begin++;
+static char _read_char(std::istream &str) {
+    if (str.eof()) {
+        throw std::runtime_error("unexpected EOF encoutered");
     }
-    if (begin == end) {
-        throw std::runtime_error("unexpected end of file");
-    }
-    char c = *begin;
-    begin++;
+    char c;
+    str.read(&c, 1);
     return c;
 }
 
-static std::string _read_string(Iterator &begin, Iterator &end) {
+static char _get_next_character(std::istream &str, State state) {
+    char c;
+    do {
+        c = _read_char(str);
+    } while (c == ' ' || c == '\t' || c == '\n');
+    return c;
+}
+
+static std::string _read_string(std::istream &str) {
     bool escaped = false;
-    std::string str;
-    while (begin != end) {
-        char c = *begin;
-        begin++;
+    std::string s;
+    while (str.good()) {
+        char c = _read_char(str);
         if (escaped) {
             escaped = false;
         } else if (!escaped && c == '\'') {
@@ -48,9 +53,9 @@ static std::string _read_string(Iterator &begin, Iterator &end) {
         } else if (!escaped && c == '"') {
             break;
         }
-        str += c;
+        s += c;
     }
-    return str;
+    return s;
 }
 
 #include <iostream>
@@ -58,7 +63,7 @@ static std::string _read_string(Iterator &begin, Iterator &end) {
 // This function parses a valid JSON object from the stream.
 // It must be entered with BEGIN pointing to the next character after
 // the opening braces.
-JSONObj _parse(Iterator &begin, Iterator &end) {
+JSONObj _parse(std::istream &str) {
     State state = KEY_START;
     std::string key;
     JSONObj value;
@@ -66,13 +71,14 @@ JSONObj _parse(Iterator &begin, Iterator &end) {
     while (state != OBJECT_END) {
         // read the next non-whitespace character from the input and compare
         // it against out expectations (i.e. if the next character would occur in valid JSON)
-        char c = _get_next_character(begin, end, state);
+        char c = _get_next_character(str, state);
         if (!_expected(c, state)) {
+            std::cout << state << std::endl;
             throw std::runtime_error("invalid character encountered '" + std::string(&c, 1) + '\'');
         }
         switch (state) {
             case KEY_START: {
-                key = _read_string(begin, end);
+                key = _read_string(str);
                 state = KEY_END;
                 break;
             }
@@ -82,9 +88,9 @@ JSONObj _parse(Iterator &begin, Iterator &end) {
             }
             case VALUE_START: {
                 if (c == '"') {
-                    value = _read_string(begin, end);
+                    value = _read_string(str);
                 } else {
-                    value = _parse(begin, end);
+                    value = _parse(str);
                 }
                 if (obj.members().find(key) != obj.members().end()) {
                     throw std::runtime_error("duplicate key encoutered '" + key + '\'');
@@ -107,11 +113,18 @@ JSONObj _parse(Iterator &begin, Iterator &end) {
 }
 
 JSONObj SimpleJSON::parse(const std::string &str) {
-    auto idx = str.find_first_of("{");
-    Iterator begin = &str[idx + 1];
-    Iterator end = str.data() + str.size();
-    if (begin == end) {
-        throw std::runtime_error("no object starting token encountered");
-    }
-    return _parse(begin, end);
+    std::stringstream ss(str);
+    return parse(ss);
+}
+
+JSONObj SimpleJSON::parse(std::istream &str) {
+    char c;
+    // check if we read all whitespace until we encounter the first opening brace
+    do {
+        c = _read_char(str);
+        if (c != '{' && c != ' ' && c != '\t' && c != '\n') {
+            throw std::runtime_error("invalid character encountered '" + std::string(&c, 1) + '\'');
+        }
+    } while (c != '{');
+    return _parse(str);
 }
